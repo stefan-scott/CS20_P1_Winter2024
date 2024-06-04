84d237adbaa2f2220916d6554d5d0dacd3ef56e7
stefan-scott <scottst@spsd.sk.ca>
2024-06-04 09:56:58 -0600

    demo done

--- 22 - Microbit Control Turtle.py
+++ 22 - Microbit Control Turtle.py
@@ -46,13 +46,46 @@ racer_b.goto(-300,-50)
 
 #Conduct the Race
     
-#CPU vs CPU
-for i in range(100):
-    racer_a.fd(random.randint(4,6))
-    racer_b.fd(random.randint(4,6))
+#CPU vs CPU    finish line at x=350
+# turtle.tracer(False)  #manage the animation ourselves...
+# while racer_a.xcor() < 350 and racer_b.xcor() < 350:
+#     racer_a.fd(random.randint(4,6))
+#     racer_b.fd(random.randint(4,6))
+#     time.sleep(0.02) #50 fps
+#     turtle.update() #refresh the screen after both moves are decided
+# if racer_a.xcor() > racer_b.xcor():
+#     print("Racer A wins!!")
+# else:
+#     print("Racer B wins!!")
 
 
+last_move = "flat"
 
+#HUMAN vs CPU
+while racer_a.xcor() < 350 and racer_b.xcor() < 350:
+    racer_a.fd(random.randint(2,3)) #CPU
+    #Human Control - accelerometer, buttons
+    
+#     x = microbit.accelerometer.get_x()  #-1000 to 1000
+#     speed = x/100
+    
+#     if microbit.button_a.was_pressed():
+#         speed = 5
+#     else:
+#         speed = 1.5
+
+#Challenge: "Handcars, Pump Trolley" Movement
+#           move each time the microbit flips from Left to Right
+#           or Right to Left
+    current_move = horizontal_tilt(300) #"right" "left" "flat"
+    if current_move != last_move and current_move != "flat":
+        speed = 15
+        last_move = current_move
+    else:
+        speed = 0
+        
+    racer_b.fd(speed)
+    
 wn.exitonclick()
     
     
